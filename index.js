0ad2f0a4dc11d747c3a8389257d922b3b1d541a6
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-12 12:11:07 -0400

    added employee js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Manager = require('./manager');
 const Engineer = require('./engineer');
+const Employee = require('./employee');
 const Intern = require('./intern');
 const inquirer = require('inquirer');
 const fd = require('fs');
