236bfa952f34448a238458323cb3a4637a9ac6a3
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-16 18:12:08 -0400

    tfix json error by redownloading npm

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,1 +1,12 @@
 const Employee = require('../lib/employee')
+
+describe("Employee details",()=>{
+    describe("Initialization",()=>{
+    it("should return an object containing name,id and email property when called with the 'new' keyword", () =>{
+        const obj = new Employee('mack','21','mack@mail.com');
+        expect(obj.id).toBe('21');
+        expect(obj.name).toBe('name');
+        expect(obj.email).toBe('mack@gmail.com')
+    })
+    
+})});
