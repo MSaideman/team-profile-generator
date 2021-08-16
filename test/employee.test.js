8b2d13ab429eb4fb1adeef60a063fd4cb4648a14
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-16 18:19:04 -0400

    add employee.test.js test 1

--- test/employee.test.js
+++ test/employee.test.js
@@ -2,11 +2,9 @@ const Employee = require('../lib/employee')
 
 describe("Employee details",()=>{
     describe("Initialization",()=>{
-    it("should return an object containing name,id and email property when called with the 'new' keyword", () =>{
-        const obj = new Employee('mack','21','mack@mail.com');
-        expect(obj.id).toBe('21');
-        expect(obj.name).toBe('name');
-        expect(obj.email).toBe('mack@gmail.com')
+    it("should return an object ", () =>{
+        const obj = new Employee();
+        expect(typeof(obj)).toBe("object");
     })
     
 })});
