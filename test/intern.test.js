12cd9e437e5f5d010fc2f69ba907829cc07d6985
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-16 18:20:37 -0400

    added intern test

--- test/intern.test.js
+++ test/intern.test.js
@@ -1,2 +1,12 @@
 const Intern = require('../lib/intern')
 
+describe("Intern details",()=>{
+    describe("Initialization",()=>{
+    it("should return an object ", () =>{
+        const obj = new Intern();
+        expect(typeof(obj)).toBe("object");
+    })
+    
+})});
+
+
