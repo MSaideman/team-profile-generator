61bf2914ab06fc9d48d2878016bcb2e5d06f3d47
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-16 18:21:02 -0400

    added manager test

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,2 +1,11 @@
 const Manager = require('../lib/manager')
 
+describe("Manager details",()=>{
+    describe("Initialization",()=>{
+    it("should return an object ", () =>{
+        const obj = new Manager();
+        expect(typeof(obj)).toBe("object");
+    })
+    
+})});
+
