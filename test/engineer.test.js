03652a5d509c332dcc1f713bf63a6ffdc98a7cd7
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-16 18:20:06 -0400

    added test for engineer.test.js

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,2 +1,11 @@
 const Engineer = require('../lib/engineer')
 
+describe("Engineer details",()=>{
+    describe("Initialization",()=>{
+    it("should return an object ", () =>{
+        const obj = new Engineer();
+        expect(typeof(obj)).toBe("object");
+    })
+    
+})});
+
